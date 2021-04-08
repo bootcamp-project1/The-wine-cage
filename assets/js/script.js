c0620768aa9e8825c1fc7feb1d23bbbc148058a8
jermz678 <jermz678@live.com>
2021-04-07 20:23:22 -0500

    created fetch request that works based on query search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+//created fetch request that works based on query search
+//will use vote-average to compare to wine 
 
-fetch("https://api.themoviedb.org/3/review/movie?api_key=9e2d992d8fb0f9588f0d380dff3225e8&query=saw")
+fetch("https://api.themoviedb.org/3/search/movie?api_key=9e2d992d8fb0f9588f0d380dff3225e8&query=Pacific+Rim")
 .then(response => {
 	console.log(response);
 })
