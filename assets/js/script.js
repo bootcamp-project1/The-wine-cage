73a4a0a73d07195eace001cfdb6ecea494eda887
P Stenger <pstenger23@gmail.com>
2021-04-08 20:34:34 -0500

    recipe api

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// var userSearchInput =
-// var movieGenre =
-// var movieRating =
-// var wineType =
-
-console.log(fetch('http://www.omdbapi.com/?i=tt3896198&apikey=2f30e3c&')
-.then(response => response.json())
-.then(data => console.log(data)));
