ddab85bd43ae82d9b19f462f83cd27f085e7e4c6
japankid-code <japankid.jake@gmail.com>
2021-04-08 21:07:17 -0500

    add TMDB actor ID search along with some notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ var searchInputVal = searchInput.value;
 //variable for button press to search
 var searchButton = document.getElementById("searchMovieBtn");
 
+// fetch from TMDB with a search term
 var searchMovieDatabase = function(){
 	var searchInputVal = searchInput.value;
 	fetch('https://api.themoviedb.org/3/search/movie?api_key=9e2d992d8fb0f9588f0d380dff3225e8&query=' + searchInputVal)
@@ -32,8 +33,21 @@ var captureSearch = function(){
 searchButton.addEventListener("click", searchMovieDatabase)
 
 
-
+// grab the recipe responses from  spoonacular
 fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=a9af3d76ab984d298de29d4837c5c9d1')
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(err => console.error(err));
+
+
+// some variables for the TMDB search by actor ID, returns list of nic cage films
+const TMDBapiKey = `9e2d992d8fb0f9588f0d380dff3225e8`;
+const nicCageID = '2963';
+
+// TMDB api fetch here, searches by actor ID number
+fetch(`https://api.themoviedb.org/3/person/${nicCageID}/movie_credits?api_key=${TMDBapiKey}`)
+.then(response => response.json())
+.then(TMDBresponse => console.log(TMDBresponse))
+.catch(err => {
+	console.error(err);
+});
