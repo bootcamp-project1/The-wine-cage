290936ae45c3476979e86df658a1da025b05e3ed
japankid-code <japankid.jake@gmail.com>
2021-04-10 17:39:22 -0500

    functionify the nic cage response :zap:

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,20 +49,24 @@ var searchMovieDatabase = function(){
 
 searchButton.addEventListener("click", searchMovieDatabase)
 
-// grab the recipe responses from  spoonacular
-fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=a9af3d76ab984d298de29d4837c5c9d1')
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(err => console.error(err));
+// // grab the recipe responses from  spoonacular
+// fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=a9af3d76ab984d298de29d4837c5c9d1')
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(err => console.error(err));
 
 // some variables for the TMDB search by actor ID, returns list of nic cage films
 const TMDBapiKey = `9e2d992d8fb0f9588f0d380dff3225e8`;
 const nicCageID = '2963';
 
 // TMDB api fetch here, searches by actor ID number
-fetch(`https://api.themoviedb.org/3/person/${nicCageID}/movie_credits?api_key=${TMDBapiKey}`)
-.then(response => response.json())
-.then(TMDBresponse => console.log(TMDBresponse))
-.catch(err => {
-	console.error(err);
-});
+const nicolasCager = (actor) => {
+	fetch(`https://api.themoviedb.org/3/person/${nicCageID}/movie_credits?api_key=${TMDBapiKey}`)
+		.then((response) => response.json())
+		.then((TMDBresponse) => {
+			console.log(TMDBresponse);
+		})
+		.catch(err => {
+			console.error(err);
+		});
+}
